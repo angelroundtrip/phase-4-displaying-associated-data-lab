2afacdad4beb224a7cbb0372bb69d44d69422238
Angel Rosario <arosario.us@gmail.com>
2022-03-16 15:01:43 -0400

    Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  # GET /users/:id
+  def show
+    user = find_user
+    render json: user, include: :items
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
+
 end
